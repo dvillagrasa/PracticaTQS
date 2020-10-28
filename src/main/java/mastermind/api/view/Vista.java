9f85510c23dbe3c7e5c752883e25c52aaa4a06c8
Daniel <daniluga97@gmail.com>
2020-10-28 10:20:45 +0100

    Comprueba que los dígitos sean entre 0 y 8 sin terminar la ejecución.

--- src/main/java/mastermind/api/view/Vista.java
+++ src/main/java/mastermind/api/view/Vista.java
@@ -8,82 +8,93 @@ import java.util.Collections;
 
 //import java.util.Scanner;
 
-/** 
+/**
  * Clase Vista - abcdefghijk
- * @author Daniel Luis Garcia 
+ * 
+ * @author Daniel Luis Garcia
  * @author Daniel Villagrasa Ramírez
- */ 
-public class Vista {//Clase Vista. Debe mostrar únicamente los datos que el controlador nos pase. Su función se limita a mostrar 
-	
+ */
+public class Vista {// Clase Vista. Debe mostrar únicamente los datos que el controlador nos pase.
+					// Su función se limita a mostrar
+
 	public static final int MAX_INTENTOS = 9;
 	public static final int MAX_CLAVIJAS = 4;
-	
+
 	private String suposicion[][];
 	private String respuesta[];
-	//private String[] combinacionSecreta;
-	
+	// private String[] combinacionSecreta;
+
 	public Vista() {
-		//obtenido = new String[]{"1","2","3","4"};
+		// obtenido = new String[]{"1","2","3","4"};
 		suposicion = new String[MAX_INTENTOS][MAX_CLAVIJAS];
 		respuesta = new String[MAX_CLAVIJAS];
-		//dibujarTablero();
+		// dibujarTablero();
 	}
-	
-	
+
 	/**
 	 * Explicación
 	 */
-	public void dibujarTablero(ArrayList<Integer> combinacionAleatoria){
-		
-		ArrayList<Integer> Combinacion = combinacionAleatoria;
-		
-		System.out.println("                ________________");
-		System.out.println(
-				"               |  " + combinacionAleatoria +
-				"  |  ");
-		System.out.println(" _______________________      _______________________");
-		 
-		for (int j = 0; j < MAX_INTENTOS; j++) {
-		      for (int k = 0; k < MAX_CLAVIJAS; k++) {
-		    	  suposicion[j][k] = "    ";
-		    	  respuesta[k] = " ";
-		      }
-		      System.out.println(
-		        "| " + suposicion[j][0] +
-		        "| " + suposicion[j][1] +
-		        "| " + suposicion[j][2] +
-		        "| " + suposicion[j][3] +
-		        "| == |  " + respuesta[0] + "  |  "+
-		        respuesta[1] + "  |  "+
-		        respuesta[2] + "  |  "+
-		        respuesta[3] + "  |  ");
-		      System.out.println(" _______________________      _______________________");
-		    }
-		
+	public void dibujarTablero(ArrayList<Integer> combinacionAleatoria) {
 
-	}
-	
-	public void PedirNumeros()
-	{
-		int MAX_INTENTOS = 9;
-		int vecesJugado = 0;
-		while(vecesJugado < MAX_INTENTOS) {
-			System.out.println("Inserta la combinación con el siguiente formato: X X X X");
-			Scanner escaner = new Scanner(System.in);
-			
-			int a[] = new int[100];
-			String s[]= escaner.nextLine().split(" ");
-			for(int i =0 ;i < s.length;i++){
-			    a[i]= Integer.parseInt(s[i]);
+		// ArrayList<Integer> Combinacion = combinacionAleatoria;
+
+		System.out.println("                ________________");
+		System.out.println("               |  " + combinacionAleatoria + "  |  ");
+		System.out.println(" _______________________      _______________________");
+
+		for (int j = 0; j < MAX_INTENTOS; j++) {
+			for (int k = 0; k < MAX_CLAVIJAS; k++) {
+				suposicion[j][k] = "    ";
+				respuesta[k] = " ";
 			}
-			for(int i =0 ; i < s.length; i++) {
-				 System.out.print(a[i]);
-			}
-			
-			vecesJugado++;
-			System.out.println("\n" + "Veces jugadas" + " " + vecesJugado);
+			System.out.println("| " + suposicion[j][0] + "| " + suposicion[j][1] + "| " + suposicion[j][2] + "| "
+					+ suposicion[j][3] + "| == |  " + respuesta[0] + "  |  " + respuesta[1] + "  |  " + respuesta[2]
+					+ "  |  " + respuesta[3] + "  |  ");
+			System.out.println(" _______________________      _______________________");
 		}
 	}
-}
 
-	
+	@SuppressWarnings("resource")
+	public void PedirNumeros() {
+		// int MAX_INTENTOS = 9;
+		int vecesJugado = 0;
+
+		while (vecesJugado < MAX_INTENTOS) {
+			try {
+				System.out.println("Inserta la combinación con el siguiente formato: X X X X");
+				Scanner escaner = new Scanner(System.in);
+
+				int combinacionJugador[] = new int[4];
+				String entradaJugador[] = escaner.nextLine().split(" ");
+
+				for (int i = 0; i < entradaJugador.length; i++) {
+					combinacionJugador[i] = Integer.parseInt(entradaJugador[i]);
+				}
+
+				for (int i = 0; i < combinacionJugador.length; i++) {
+					if ((combinacionJugador[i] < 0) || (combinacionJugador[i] > 8)) {
+						throw new Exception("El número " +combinacionJugador[i] + " está fuera del rango [0 - 8].");
+					} else {
+						System.out.println("El número " + combinacionJugador[i] + " es válido.");
+					}
+
+				}
+				vecesJugado++;
+				System.out.println("\n" + "Número de jugadas: " + vecesJugado);
+				// escaner.close();
+			} catch (NumberFormatException e) {
+				System.out.println("La combinación debe de ser numérica.");
+			
+			} catch (Exception e) {
+				System.out.println(e);
+			}
+
+		}
+
+	}
+
+	private void print(String string) {
+		// TODO Auto-generated method stub
+
+	}
+}
