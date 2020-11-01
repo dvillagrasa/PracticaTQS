bff774962b093b6c3d85afa19b235be9a05238af
Daniel <daniluga97@gmail.com>
2020-11-01 14:23:53 +0100

    Actualiza la función testgenerarCombinacionAleatoria()
    El código anterior no era suficientemente eficiente. El actual debería serlo.

--- src/test/java/mastermind/api/model/ModeloTest.java
+++ src/test/java/mastermind/api/model/ModeloTest.java
@@ -22,17 +22,21 @@ public class ModeloTest {
 	@Test
 	public void testgenerarCombinacionAleatoria() {
 		boolean esRepetido = false;
-
 		char[][] resultado = Modelo.generarCombinacionAleatoria();
-		//char[][] resultado = {'1', '2' , '3' , '5'}; //Array de carácteres para comprobar que sí funciona el test.
+		//char[][] resultado = {{'5'},{'4'},{'0'},{'0'}}; //Array de carácteres para comprobar que sí funciona el test.
+		
+		//System.out.println(Arrays.deepToString(resultado)); Imprime [[5], [4], [0], [0]]
+		
 		for (int i = 0; i < resultado.length; i++) { 
 			for (int j = i + 1 ; j < resultado.length; j++) { 
-				if(resultado[i] == resultado[j]) {
-					//System.out.println("Repetido: " + resultado[i]);
+				//System.out.println("tstI " + Arrays.toString(resultado[i])); Imprime [5] (si i = 0)
+				//System.out.println("tstJ " + Arrays.toString(resultado[j])); Imprime [4] (si j = 1)
+				if(resultado[i][0] == resultado[j][0]) {
+					//System.out.println("Repetido: " + Arrays.toString(resultado[i])); //Imprime cuál es el número repetido (en este caso el [0])
 					esRepetido = true;
 				}
 			}
-		assertFalse(esRepetido);//Esperamos que sea falso, es decir, que no encuentre ninguna repetición.
+		assertFalse(esRepetido); //Esperamos que sea falso, es decir, que no encuentre ninguna repetición.
 		} 
 	}
 	
