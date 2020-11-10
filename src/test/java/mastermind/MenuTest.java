eebfe265a0c95acaac9de469bcda9168e7a8f256
Daniel <daniluga97@gmail.com>
2020-11-10 13:50:34 +0100

    Crea tests para el método comprobarOpción()
    - Valores límite
    - Valores frontera
    - Valores muy alejados del límite

--- src/test/java/mastermind/MenuTest.java
+++ src/test/java/mastermind/MenuTest.java
@@ -7,8 +7,19 @@ import org.junit.Test;
 public class MenuTest {
 
 	@Test
-	public void testMostrarMenu() {
-		fail("Not yet implemented");
-	}
+	public void testComprobarOpcion() {
+		
+		Menu menu = new Menu();
+		
+		assertFalse(menu.comprobarOpcion(-50)); // Límite inferior extremo.
+		assertFalse(menu.comprobarOpcion(-1)); // Límite inferior alejado.
+		assertFalse(menu.comprobarOpcion(0)); // Frontera inferior.
+		
+		assertTrue(menu.comprobarOpcion(1)); // Frontera inferior.
+		assertTrue(menu.comprobarOpcion(2)); // Frontera superior.
+		
+		assertFalse(menu.comprobarOpcion(3)); // Límite superior alejado.
+		assertFalse(menu.comprobarOpcion(50)); // Límite superior extremo.
 
+	}
 }
