eb5c1f173124204e35ce3125772b1adfad32d859
Daniel <daniluga97@gmail.com>
2020-11-10 13:51:25 +0100

    Crea el método comprobarOpción()

--- src/main/java/mastermind/Menu.java
+++ src/main/java/mastermind/Menu.java
@@ -1,5 +1,25 @@
 package mastermind;
 
 public class Menu {
-
+	
+	void mostrarMenu() {
+		
+		System.out.println("---------- MASTER MIND ----------");
+		System.out.println("---------------------------------");
+		System.out.println("- [1] - Jugar");
+		System.out.println("- [2] - Salir");
+		System.out.println("---------------------------------");
+		System.out.println("");
+		System.out.println("Selecciona una opción (1 o 2).");
+	}
+	
+	
+	public boolean comprobarOpcion(int opcion) {
+		boolean esCorrecto = true;
+		
+		if((opcion < 1) || (opcion > 2)) {
+			esCorrecto = false;
+		}
+		return esCorrecto;
+	}
 }
