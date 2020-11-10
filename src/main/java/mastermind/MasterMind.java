738fd1da6a0fd5cdee29438a1ab8b0e45654918a
Daniel <daniluga97@gmail.com>
2020-11-10 14:03:12 +0100

    Actualiza la clase principal del juego
    Incorpora el menú 100% funcional después de haber realizado su test de forma previa.

--- src/main/java/mastermind/MasterMind.java
+++ src/main/java/mastermind/MasterMind.java
@@ -1,8 +1,9 @@
 package mastermind;
 
+import mastermind.api.model.*;
 import mastermind.api.view.*;
 import mastermind.api.controller.*;
-import mastermind.api.model.*;
+import java.util.Scanner;
 
 /** 
  * Clase MasterMind - Clase principal del juego Master Mind que contiene el método principal del juego.
@@ -13,14 +14,37 @@ import mastermind.api.model.*;
 public class MasterMind {
 	
 	/**
-	 * Método principal del juego Master Mind. Aquí se generan llamadas a los constructores de las clases
-	 * Vista, Modelo y Controlador.
+	 * Método principal del juego Master Mind.
 	 * 
 	 * @param args - Array de cadena de carácteres con los argumentos introducidos desde la línea de comandos.
 	 */
 	public static void main(String[] args) {
-		Vista vista = new Vista();
-		Modelo modelo = new Modelo();
-		Controlador controlador = new Controlador(vista, modelo);
+		
+		Menu menu = new Menu();
+		boolean esCorrecta = false;
+		
+		do {
+			menu.mostrarMenu();
+			int opcion;
+			Scanner escaner = new Scanner(System.in);
+			opcion = escaner.nextInt();
+			
+			esCorrecta = menu.comprobarOpcion(opcion);
+			
+			switch(opcion) {
+				case 1:
+					Vista vista = new Vista();
+					Modelo modelo = new Modelo();
+					Controlador controlador = new Controlador(vista, modelo);
+					break;
+				case 2:
+					System.out.println("¡Hasta pronto!");
+					System.exit(0);
+					break;
+				default:
+					System.out.println("Opción inválida, inténtalo de nuevo.");
+					break;
+			}
+		}while(esCorrecta);
 	}
 }
