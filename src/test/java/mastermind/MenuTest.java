584829cf0dca25af5cd74b8db1cec6cda634559c
Daniel <daniluga97@gmail.com>
2020-11-11 11:25:02 +0100

    Actualiza MenuTest
    Añade nuevos tests para el método comprobarOpcion().

--- src/test/java/mastermind/MenuTest.java
+++ src/test/java/mastermind/MenuTest.java
@@ -5,20 +5,28 @@ import static org.junit.Assert.*;
 import org.junit.Test;
 
 public class MenuTest {
-
+	/**
+	 * Test del método comprobarOpción.
+	 */
 	@Test
 	public void testComprobarOpcion() {
 		
 		Menu menu = new Menu();
 	
-		assertFalse(menu.comprobarOpcion('-')); // Límite inferior alejado.
-		assertFalse(menu.comprobarOpcion('0')); // Frontera inferior.
+		assertFalse(menu.comprobarOpcion('-')); // Límite.
+		assertFalse(menu.comprobarOpcion('*')); // Límite.
+		
+		assertFalse(menu.comprobarOpcion('0')); // Límite inferior.
 		
 		assertTrue(menu.comprobarOpcion('1')); // Frontera inferior.
 		assertTrue(menu.comprobarOpcion('2')); // Frontera superior.
 		
 		assertFalse(menu.comprobarOpcion('3')); // Límite superior alejado.
-		assertFalse(menu.comprobarOpcion('9')); // Límite superior extremo.
-
+		assertFalse(menu.comprobarOpcion('4')); // Límite superior alejado.
+		assertFalse(menu.comprobarOpcion('5')); // Límite superior alejado.
+		assertFalse(menu.comprobarOpcion('6')); // Límite superior alejado.
+		assertFalse(menu.comprobarOpcion('7')); // Límite superior alejado.
+		assertFalse(menu.comprobarOpcion('8')); // Límite superior alejado.
+		assertFalse(menu.comprobarOpcion('9')); // Límite superior alejado.
 	}
 }
